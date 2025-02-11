db42a847e9c3c004722e2cfb377fca5b55644b54
Wieske <wieskedeswart@gmail.com>
2025-02-11 11:31:37 +0100

    Revision: update figure settings and add new figures
    New figures:
    - metrics for true probabilities
    - results of new simulation scenario
    - comparison to random search
    - overview of training times

--- paper_figures.py
+++ paper_figures.py
@@ -1,81 +1,130 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from skeleton.results_analysis import combine_files, landmark_subplots
+from skeleton.results_analysis import combine_files, landmark_subplots, train_times_plot
+import ast
 
+fig_dir = "results/figures"
+sup = False  # Create and save figures for supplementary material
 
-fig_dir = "figures"
-sup = False  # create supplementary figures
-plt.rcParams.update({'font.size': 12})
+# Set style parameters
+plt.rc('axes', titlesize=12, labelsize=12, grid=True)
+plt.rcParams["axes.grid.axis"] = "y"
+plt.rc('figure', figsize=[11, 8], labelsize=12, titlesize=14)
+plt.rc('font', size=12)
+plt.rc('legend', fontsize=12)
+plt.rc("lines", markersize=6)
+plt.rc('xtick', labelsize=12)
+plt.rc('ytick', labelsize=12)
 
-# Combine results from experiments into one file
+landmarking = {"0": "No", "1": "Super", "2": "Random", "3": "Strict"}
+
+# Load and combine experiment results
+# Run these once to combine all separate metric files of an experiment into one file
 # logs_dir = "logs/"
-# combine_files([f"{logs_dir}/Landmarking_s{n}_1000" for n in [1, 2, 3]], savename="landmarking_sim", task_lib=landmarking)
+# combine_files([f"{logs_dir}/Landmarking_s{n}_1000" for n in [1, 2, 3, 4]], savename="landmarking_sim", task_lib=landmarking)
 # combine_files([f"{logs_dir}/Landmarking_ADNI"], savename="landmarking_ADNI", task_lib=landmarking)
+# combine_files([f"{logs_dir}/Randomsearch_ADNI"], savename="Randomsearch_ADNI", task_lib={3: "Random search"})
 
-# Load simulation results
-m_sim = pd.read_csv(f"results/Metrics_landmarking_sim.csv", header=[0, 1])
-m_sim = m_sim.rename(columns=lambda x: "" if "Unnamed" in x else x, level=1)
+
+def read_results(filename):
+    """Load .csv file containing metric results"""
+    df = pd.read_csv(filename, header=[0, 1])
+    df = df.rename(columns=lambda x: "" if "Unnamed" in x else x, level=1)
+    if "ADNI" in filename:
+        # convert months to years in adni for consistency:
+        df = df.rename(columns=lambda x: int(x) / 12 if x in ["12", "24", "36", "48", "60"] else x, level=1)
+        df["landmark"] = df["landmark"] / 12
+    return df
+
+
+# Read results data
+m_sim = read_results("results/Metrics_landmarking_sim.csv")
 m_s3 = m_sim[m_sim["dataset"] == "s3"]
+m_adni = read_results("results/Metrics_landmarking_ADNI.csv")
+m_adni_rs = read_results("results/Metrics_Randomsearch_ADNI.csv")
 
-# Load adni results
-m_adni = pd.read_csv(f"results/Metrics_landmarking_ADNI.csv", header=[0, 1])
-m_adni = m_adni.rename(columns=lambda x: "" if "Unnamed" in x else x, level=1)
-# convert months to years in adni for consistency:
-m_adni = m_adni.rename(columns=lambda x: int(x)/12 if x in ["12", "24", "36", "48", "60"] else x, level=1)
-m_adni["landmark"] = m_adni["landmark"] / 12
+# get average scores on adni for quantitative results in abstract:
+adni_avg = m_adni.groupby(["long_model", "surv_model", "task"]).agg(auc_mean=(("auc", "avg"), "mean"),
+                                                                    auc_std=(("auc", "avg"), "std"),
+                                                                    bs_mean=(("brier_score", "avg"), "mean"),
+                                                                    bs_std=(("brier_score", "avg"), "std"))
+
+# True results:
+df = m_sim[(m_sim["task"] == "Strict") & (m_sim["long_model"] == "baseline") & (m_sim["surv_model"] == "True")]
+landmark_subplots(df, "metric", "dataset", metric=["auc", "brier_score"],
+                  title="tdAUC and brier score obtained by the true survival probabilities",
+                  saveto=f"{fig_dir}/true_metrics.jpg")
 
 # MSE across different synthetic datasets
-df = m_sim[(m_sim["task"] == "Strict")]
+df = m_sim[(m_sim["task"] == "Strict") & (m_sim["dataset"].isin(["s1", "s2", "s3"]))]
 models = [("baseline", "CPH"), ("baseline", "RSF"), ("last_visit", "RSF"), ("RNN", "FNN")]
-f = landmark_subplots(df, "dataset", models, metric="mse", title="MSE across different synthetic datasets")
-f.savefig(f"{fig_dir}/sim_scenarios.jpg")
+landmark_subplots(df, "dataset", models, metric="mse", title="MSE across different synthetic datasets",
+                  saveto=f"{fig_dir}/sim_scenarios.jpg")
+
+df = m_sim[(m_sim["task"] == "Strict") & (m_sim["dataset"].isin(["s4"]))]
+models = [("last_visit", "RSF"), ("MFPCA", "RSF"), ("RNN_long", "FNN"), ("RNN", "FNN")]
+landmark_subplots(df, "metric", models, metric=["mse", "auc"], title="MSE and tdAUC on synthetic datasets 4",
+                  saveto=f"{fig_dir}/sim_scenarios4.jpg")
 
 # Average tdAUC with different landmarking methods
 df = m_s3[(m_s3["task"] != "Random") & (m_s3["surv_model"].isin(["CPH", "RSF"])) &
           (m_s3["long_model"].isin(["last_visit", "MFPCA", "RNN"]))]
-f = landmark_subplots(df, "surv_model", "long_model", "task", metric="mse",
-                      title="Average tdAUC with different landmarking methods")
-f.savefig(f"{fig_dir}/Landmarking.jpg")
+landmark_subplots(df, "surv_model", "long_model", "task", metric="mse",
+                  title="Average tdAUC with different landmarking methods", saveto=f"{fig_dir}/Landmarking.jpg")
 
 # RNN results at landmark time: 3 years
 df = m_s3[m_s3["landmark"] == 3]
-f = landmark_subplots(df, "metric", [("RNN", "FNN"), ("RNN_long", "FNN")], "task",
-                      metric=["auc", "mse"],
-                      title="RNN results at landmark time: 3 years")
-f.savefig(f"{fig_dir}/RNN_lm3.jpg")
+landmark_subplots(df, "metric", [("RNN", "FNN"), ("RNN_long", "FNN")], "task", metric=["auc", "mse"],
+                  title="RNN results at landmark time: 3 years", saveto=f"{fig_dir}/RNN_lm3.jpg")
 
 # ADNI results
-f = landmark_subplots(m_adni, "surv_model", "long_model", "task", metric="auc",
-                      title="Average tdAUC with different landmarking methods on ADNI")
-f.savefig(f"{fig_dir}/LM_adni.jpg")
+landmark_subplots(m_adni, "surv_model", "long_model", "task", metric="auc",
+                  title="Average tdAUC with different landmarking methods on ADNI", saveto=f"{fig_dir}/LM_adni.jpg")
 
 # ADNI results at landmark time: 3 years
 df = m_adni[m_adni["landmark"] == 3]
-f = landmark_subplots(df, "metric", [("last_visit", "RSF"), ("RNN_long", "RSF"), ("RNN_long", "FNN")],
-                      plot_var="task", metric=["auc", "brier_score"], title="ADNI results at landmark time: 3 years")
-f.savefig(f"{fig_dir}/ADNI_lm3.jpg")
+landmark_subplots(df, "metric", [("last_visit", "RSF"), ("RNN_long", "RSF"), ("RNN_long", "FNN")],
+                  plot_var="task", metric=["auc", "brier_score"], title="ADNI results at landmark time: 3 years",
+                  saveto=f"{fig_dir}/ADNI_lm3.jpg")
+
+# Comparison to random search:
+adni_fixed = m_adni[(m_adni["landmark"] == 3) & (m_adni["task"] == "Strict")].copy()
+adni_fixed["task"] = "Fixed parameters"
+df = pd.concat([m_adni_rs, adni_fixed])
+landmark_subplots(df, "metric", [("last_visit", "RSF"), ("RNN_long", "RSF"), ("RNN_long", "FNN"), ("RNN", "FNN")],
+                  plot_var="task", metric=["auc", "brier_score"], title="ADNI results at landmark time: 3 years",
+                  saveto=f"{fig_dir}/ADNI_RS_vs_fixed.jpg")
+
+# Training times
+par = pd.read_csv("results/Params_landmarking_ADNI.csv", index_col=["task", "long_model", "run_nr"])
+train_times = par["train_times"].map(lambda x: ast.literal_eval(x))
+mean_times = (train_times.groupby(["task", "long_model"]).apply(lambda x: pd.DataFrame(x.tolist()).mean())
+              .rename(index=lambda x: x if x in ["CPH", "RSF", "FNN"] else "long_training", level=2)
+              .unstack(level=2).swaplevel(0, 1, axis=0).sort_index())
+train_times_plot(mean_times, saveto=f"{fig_dir}/ADNI_traintimes.jpg")
+
 
 # Supplementary results
 if sup:
-    f = landmark_subplots(m_s3, "landmark", [("RNN", "FNN"), ("RNN_long", "FNN")], "task", metric="auc",
-                          title="Comparison of neural network methods")
-    f.savefig(f"{fig_dir}/Supplementary/RNN_vs_RNN_long.jpg")
-    x_col = "task"
-    landmarking = sorted(df[x_col].unique(), key=lambda x: x.lower() if isinstance(x, str) else x)
+    plt.rc('figure', figsize=[11, 7], labelsize=12, titlesize=14)
+    landmark_subplots(m_s3, "landmark", [("RNN", "FNN"), ("RNN_long", "FNN")], "task", metric="auc",
+                      title="Comparison of neural network methods",
+                      saveto=f"{fig_dir}/Supplementary/RNN_vs_RNN_long.jpg")
+    landmarking = ['No', 'Random', 'Strict', 'Super']
     for i, xc in enumerate(landmarking):
         # simulation plots
         df = m_s3[(m_s3["task"] == xc) & (m_s3["surv_model"] != "True")]
-        title = f"MSE for simulation scenario 3 with {xc} landmarking"
-        f = landmark_subplots(df, "surv_model", "long_model", metric="mse", title=title)
-        f.savefig(f"{fig_dir}/Supplementary/s3_{xc}_mse.png")
-        title = f"AUC for simulation scenario 3 with {xc} landmarking"
-        f = landmark_subplots(df, "surv_model", "long_model", metric="auc", title=title)
-        f.savefig(f"{fig_dir}/Supplementary/s3_{xc}_auc.png")
+        landmark_subplots(df, "surv_model", "long_model", metric="mse",
+                          title=f"MSE for simulation scenario 3 with {xc} landmarking",
+                          saveto=f"{fig_dir}/Supplementary/s3_{xc}_mse.png")
+        landmark_subplots(df, "surv_model", "long_model", metric="auc",
+                          title=f"AUC for simulation scenario 3 with {xc} landmarking",
+                          saveto=f"{fig_dir}/Supplementary/s3_{xc}_auc.png")
         # ADNI plots
         df = m_adni[(m_adni["task"] == xc)]
-        title = f"tdAUC for ADNI data with {xc} landmarking"
-        f = landmark_subplots(df, "surv_model", "long_model", metric="auc", title=title)
-        f.savefig(f"{fig_dir}/Supplementary/ADNI_{xc}_auc.png")
-        title = f"Brier score for ADNI data with {xc} landmarking"
-        f = landmark_subplots(df, "surv_model", "long_model", metric="brier_score", title=title)
-        f.savefig(f"{fig_dir}/Supplementary/ADNI_{xc}_bs.png")
+        landmark_subplots(df, "surv_model", "long_model", metric="auc",
+                          title=f"tdAUC for ADNI data with {xc} landmarking",
+                          saveto=f"{fig_dir}/Supplementary/ADNI_{xc}_auc.png")
+        landmark_subplots(df, "surv_model", "long_model", metric="brier_score",
+                          title=f"Brier score for ADNI data with {xc} landmarking",
+                          saveto=f"{fig_dir}/Supplementary/ADNI_{xc}_bs.png")
--- skeleton/results_analysis.py
+++ skeleton/results_analysis.py
@@ -1,124 +1,163 @@
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def load_metrics(fdir, **kwargs):
+    """
+    Load and concatenate .csv files containing metric results
+    @param fdir: directory containing (only) .csv files containing metric results
+    @param kwargs: additional arguments that should be added to metrics
+    @return: dataframe with concatenated metrics
+    """
     dfs = []
     for f in os.scandir(fdir):
-        name = f.name.split(".")[0].split("_")
-        metrics = pd.read_csv(f.path, header=[0, 1], index_col=0).reset_index(names="landmark")
-        metrics["n"] = name[-1]
-        if name[0] == "last" or name[1] == "long":
-            metrics["long_model"] = "_".join(name[:2])
-            metrics["surv_model"] = name[2]
-        else:
-            metrics["long_model"] = name[0]
-            metrics["surv_model"] = name[1]
-        for k, v in kwargs.items():
-            metrics[k] = v
-        dfs.append(metrics)
+        if f.is_file():
+            name = f.name.split(".")[0].split("_")
+            metrics = pd.read_csv(f.path, header=[0, 1], index_col=0).reset_index(names="landmark")
+            if name[0].startswith("test"):
+                # indicates hyperparameter search is used, where the last part of the name will indicate which
+                # validation run is chosen for test evaluation ("_p#"), save this and remove from name
+                metrics["p"] = name[-1][1:]
+                name = name[1:-1]
+            metrics["n"] = name[-1]
+            # extract longitudinal and survival model from name
+            if name[0] == "last" or name[1] == "long":
+                metrics["long_model"] = "_".join(name[:2])
+                metrics["surv_model"] = name[2]
+            else:
+                metrics["long_model"] = name[0]
+                metrics["surv_model"] = name[1]
+            for k, v in kwargs.items():
+                metrics[k] = v
+            dfs.append(metrics)
     return pd.concat(dfs)
 
 
-def load_param(directory, long_models, name):
-    params = {}
-    for long_model in long_models:
-        try:
-            param = pd.read_csv(f"{directory}/params_{long_model}.csv", index_col=0)
-            params[name] = param
-        except FileNotFoundError:
-            print(f"File {directory}/params_{long_model}.csv not found.")
+def load_param(directory):
+    """
+    Load and concatenate .csv files containing parameter settings
+    @param directory: directory containing parameter files
+    @return: dataframe containing parameter settings of all models
+    """
+    params = []
+    for f in os.scandir(directory):
+        if f.name.startswith("param"):
+            param = pd.read_csv(f.path, index_col=0)
+            params.append(param)
     if len(params) > 0:
-        return pd.concat(params, axis=1)
+        return pd.concat(params, axis=0)
 
 
 def combine_files(dirs: list, savename=None, task_lib=None):
-    long_models = ["baseline", "last_visit", "MFPCA", "RNN", "RNN_long"]
-    param = []
+    """
+    Combine multiple .csv files into a single .csv file for metrics as well as parameters
+    @param dirs: list of directories that should be searched for files
+    @param savename: (optional) name to use for saving results, if no name is provided results are not saved
+    (will save to results/Metrics_{savename}.csv and results/Parameters_{savename}.csv)
+    @param task_lib: (optional) dictionary containing numbers and names for the tasks that should be included,
+    if not provided these will be inferred from the directories in dirs
+    @return: metrics, params
+    """
+    param = {}
     metrics = []
     for project_dir in dirs:
+        # extract dataset and train size from directory name
         project = project_dir.split("/")[-1]
         dataset = project.split("_")[1]
         train_size = project.split("_")[-1]
-        for d in os.scandir(project_dir):
-            task_nr = d.name.split("_")[-1]
-            task = task_nr if task_lib is None else task_lib[task_nr]
-            param.append(load_param(d.path, long_models, name=f"{project}_{task}"))
-            metrics.append(load_metrics(f"{d.path}/metrics", dataset=dataset, train_size=train_size, task=task))
+        if task_lib is None:
+            for d in os.scandir(project_dir):
+                task = d.name.split("_")[-1]
+                param[task] = load_param(d.path)
+                metrics.append(load_metrics(f"{d.path}/metrics", dataset=dataset, train_size=train_size, task=task))
+        else:
+            for task_nr, task in task_lib.items():
+                tdir = f"{project_dir}/task_{task_nr}"
+                param[task] = load_param(tdir)
+                metrics.append(load_metrics(f"{tdir}/metrics", dataset=dataset, train_size=train_size, task=task))
     metrics = pd.concat(metrics)
-    params = pd.concat(param, axis=1)
+    params = pd.concat(param, axis=0, names=["task", "run_nr"])
     if savename is not None:
         metrics.to_csv(f"results/Metrics_{savename}.csv", index=False)
         params.to_csv(f"results/Params_{savename}.csv")
     return metrics, params
 
 
-def metric_avg(df, ax, task, metric, colors=None):
+def get_label(v):
+    if isinstance(v, (int, float)):
+        return f"landmark {v}"
+    elif v in ["No", "Super", "Random", "Strict"]:
+        return f"{v} landmarking"
+    else:
+        return str(v)
+
+
+def metric_avg(df, ax, task, metric, colors=None, markers=None):
     df = df.set_index("landmark")
     task_list = sorted(df[task].unique(), key=lambda x: x.lower() if isinstance(x, str) else x)
-    ax.grid(visible=True, which="major", axis="y")
     for t in task_list:
         y = df.loc[(df[task] == t), (metric, "avg")]
         y = y.groupby(y.index).agg(["mean", "std"])
-        if colors is None:
-            ax.errorbar(y.index, y["mean"], fmt='o:', yerr=y["std"], elinewidth=0.7, label=f"{t} landmarking")
-        else:
-            ax.errorbar(y.index, y["mean"], fmt='o:', yerr=y["std"], elinewidth=0.7, label=f"{t} landmarking", color=colors[t])
+        c = None if colors is None else colors[t]
+        m = None if markers is None else markers[t]
+        ax.errorbar(y.index, y["mean"], fmt=':', yerr=y["std"], elinewidth=0.7, label=get_label(t), color=c, marker=m)
     return ax
 
 
-def lm_over_time(df, ax, colors=None):
+def lm_over_time(df, ax, colors=None, markers=None):
     df = df.drop(columns="avg")
-    ax.grid(visible=True, which="major", axis="y")
     for lm in sorted(df.index.unique()):
         x = pd.to_numeric(df.columns) + lm
         y = df.loc[lm]
-        if colors is None:
-            ax.errorbar(x, y.mean(), yerr=y.std(), elinewidth=0.7, label=f"landmark {lm}")
-        else:
-            ax.errorbar(x, y.mean(), yerr=y.std(), elinewidth=0.7, label=f"landmark {lm}", color=colors[lm])
+        c = None if colors is None else colors[lm]
+        m = None if markers is None else markers[lm]
+        ax.errorbar(x, y.mean(), yerr=y.std(), elinewidth=0.7, label=get_label(lm), color=c, marker=m)
     return ax
 
 
-def task_over_time(df, ax, lm, colors=None):
+def task_over_time(df, ax, lm, colors=None, markers=None):
     df = df.drop(columns="avg")
-    ax.grid(visible=True, which="major", axis="y")
     for t in sorted(df.index.unique()):
         x = pd.to_numeric(df.columns) + lm
         y = df.loc[t]
-        if colors is None:
-            ax.errorbar(x, y.mean(), yerr=y.std(), elinewidth=0.7, label=f"{t} landmarking")
-        else:
-            ax.errorbar(x, y.mean(), yerr=y.std(), elinewidth=0.7, label=f"{t} landmarking", color=colors[t])
+        c = None if colors is None else colors[t]
+        m = None if markers is None else markers[t]
+        ax.errorbar(x, y.mean(), yerr=y.std(), elinewidth=0.7, label=get_label(t), color=c, marker=m)
     return ax
 
 
-def landmark_subplots(df, x_col, y_col, plot_var="landmark", metric="auc", title=None):
+def sort_varlist(df, col):
+    if isinstance(col, str):
+        l = sorted(df[col].unique(), key=lambda x: x.lower() if isinstance(x, str) else x)
+        if l == ["CPH", "FNN", "RSF"]:
+            l = ["CPH", "RSF", "FNN"]
+    else:
+        l = col
+    return l
+
+
+def landmark_subplots(df, x_col, y_col, plot_var="landmark", metric="auc", title=None, saveto=None):
     if x_col == "metric":
         x_list = metric
-    elif isinstance(x_col, str):
-        x_list = sorted(df[x_col].unique(), key=lambda x: x.lower() if isinstance(x, str) else x)
     else:
-        x_list = x_col
-    if isinstance(y_col, str):
-        y_list = sorted(df[y_col].unique(), key=lambda x: x.lower() if isinstance(x, str) else x)
-    else:
-        y_list = y_col
+        x_list = sort_varlist(df, x_col)
+    y_list = sort_varlist(df, y_col)
     nx, ny = len(x_list), len(y_list)
-    fig, axs = plt.subplots(nx, ny, squeeze=False, sharey="row", figsize=(4*ny+5, 3*nx+5))
-    colorlist = ["orange", "teal", "aqua", "sienna"]
-    colordict = dict(zip(sorted(df[plot_var].unique()), colorlist))
+    fig, axs = plt.subplots(nx, ny, squeeze=False, sharey="row")
+    colordict = dict(zip(sorted(df[plot_var].unique()), ["orange", "teal", "aqua", "sienna"]))
+    markerdict = dict(zip(sorted(df[plot_var].unique()), ["o", "v", "x", "d"]))
     for i, xc in enumerate(x_list):
         if x_col == "metric":
             metric = xc
             xc = None
         metric_label = {"auc": "tdAUC", "mse": "MSE", "brier_score": "Brier score"}
-        axs[i, 0].set_ylabel(metric_label[metric], fontsize="large")
+        axs[i, 0].set_ylabel(metric_label[metric])
         if metric in ["auc", "c_index"]:
-            axs[i, 0].set_ylim(bottom=0.5, top=1)
+            axs[i, 0].set_ylim(bottom=0.7, top=1)
         elif metric in ["mse", "brier_score"]:
-            axs[i, 0].set_ylim(bottom=0, top=0.25)
+            axs[i, 0].set_ylim(bottom=0, top=0.2)
 
         for j, yc in enumerate(y_list):
             letter = chr(ord('A') + (i*len(y_list) + j))
@@ -126,6 +165,7 @@ def landmark_subplots(df, x_col, y_col, plot_var="landmark", metric="auc", title
             name = f"{yc} - {xc}"
             if xc is None:
                 cond1 = True
+                name = f"{yc}"
             elif isinstance(x_col, str):
                 cond1 = (df[x_col] == xc)
             else:
@@ -141,21 +181,71 @@ def landmark_subplots(df, x_col, y_col, plot_var="landmark", metric="auc", title
 
             # create plot:
             if len(subset) > 0:
-                axs[-1, j].set_xlabel("prediction time (years)", fontsize="large")
+                xlabel = "prediction time (years)"
                 if plot_var == "landmark":
                     subset = subset.set_index("landmark")
-                    axs[i, j] = lm_over_time(subset.loc[:, metric], axs[i, j], colors=colordict)
+                    axs[i, j] = lm_over_time(subset.loc[:, metric], axs[i, j], colors=colordict, markers=markerdict)
                 elif (x_col == "metric") or ("landmark" in [x_col, y_col]):
                     subset = subset.set_index(plot_var)
                     lm = df["landmark"].iloc[0]
-                    axs[i, j] = task_over_time(subset.loc[:, metric], axs[i, j], lm, colors=colordict)
+                    axs[i, j] = task_over_time(subset.loc[:, metric], axs[i, j], lm, colors=colordict, markers=markerdict)
                 else:
-                    axs[i, j] = metric_avg(subset, axs[i, j], plot_var, metric, colors=colordict)
-                    axs[-1, j].set_xlabel("landmark time (years)", fontsize="large")
+                    axs[i, j] = metric_avg(subset, axs[i, j], plot_var, metric, colors=colordict, markers=markerdict)
+                    xlabel = "landmark time (years)"
+                axs[-1, j].set_xlabel(xlabel)
             else:
                 axs[i, j].axis("off")
                 axs[i, j].set_title("")
+                axs[i-1, j].set_xlabel(xlabel)
+                if j < len(y_list):
+                    axs[i, j+1].set_ylabel(metric_label[metric])
     handles, labels = axs[-1, -1].get_legend_handles_labels()
-    fig.legend(handles, labels, loc="center", bbox_to_anchor=(0.5, 0.93), ncols=len(labels), fontsize="large")
-    fig.suptitle(f"{title}", fontsize="x-large")
+    fig.suptitle(f"{title}")
+    fig.legend(handles, labels, loc="center", bbox_to_anchor=(0.5, 0.95), ncols=len(labels))
+    fig.set_layout_engine(layout="constrained", rect=(0, 0, 1, 0.95))
+    if saveto is None:
+        fig.show()
+    else:
+        fig.savefig(saveto, bbox_inches='tight', dpi=300)
+        plt.close()
+
+
+def train_times_plot(mean_times, saveto=None):
+    fig, axs = plt.subplots(5, 1, sharex="col")
+    fig.suptitle("Average training times on the ADNI dataset")
+    for i, lm in enumerate(['baseline', 'last_visit', 'MFPCA', 'RNN', 'RNN_long']):
+        subset = mean_times.xs(lm, level="long_model")
+        if "RNN" not in lm:
+            subset = subset
+        y_pos = np.arange(len(subset)) + 0.05
+        axs[i].barh(y_pos, width=subset["long_training"], height=0.9, align="edge", color="orange",
+                    label="Longitudinal model")
+        axs[i].set_yticks(y_pos + 0.45, [s + " landmarking" for s in subset.index])
+        axs[i].set_title(lm)
+        surv_models = ["FNN", "RSF", "CPH"] if "RNN" in lm else ["RSF", "CPH"]
+        cdict = {"FNN": "teal", "RSF": "aqua", "CPH": "sienna"}
+        h = 0.9 / len(surv_models)
+        for j, sm in enumerate(surv_models):
+            axs[i].barh(y_pos + j * h, width=subset[sm], left=subset["long_training"], height=h, align="edge",
+                        color=cdict[sm], label=f"{sm}")
+    axs[-1].set_xlabel("Training time (seconds)")
+    handles, labels = axs[-1].get_legend_handles_labels()
+    fig.legend(handles, labels, loc="center", bbox_to_anchor=(0.5, 0.95), ncols=len(labels))
+    fig.set_layout_engine(layout="constrained", rect=(0, 0, 1, 0.95))
+    if saveto is None:
+        fig.show()
+    else:
+        fig.savefig(saveto, bbox_inches='tight', dpi=300)
+        plt.close()
+
+
+def plot_event_dist(ids):
+    x = ids["event_time"].groupby(ids["event"]).value_counts().unstack(level=0).fillna(value=0)
+    fig, ax = plt.subplots()
+    ax.bar(x.index, x.loc[:, 0], width=2, label="Censored", bottom=np.zeros(len(x)))
+    ax.bar(x.index, x.loc[:, 1], width=2, label="Event", bottom=x.loc[:, 0])
+    ax.set_xlabel("Event time (months)")
+    ax.set_ylabel("Count")
+    fig.suptitle("ADNI event distribution")
+    fig.legend(loc="right")
     return fig
